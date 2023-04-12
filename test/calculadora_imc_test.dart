a01d1bd9433d962e5ecb7b6f60ab87e4bdb3cd80
Eduardo Cabral Costa <eduardocabral95@gmail.com>
2023-04-12 10:25:02 -0300

    Classe Teste

--- test/calculadora_imc_test.dart
+++ test/calculadora_imc_test.dart
@@ -1,8 +1,13 @@
-import 'package:calculadora_imc/classes/Usuario.dart';
+import 'package:calculadora_imc/classes/usuario.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(42, 42);
+  test('Nome de usuário', () {
+    Usuario pessoaTeste = Usuario();
+    pessoaTeste.setNome('Edu');
+    pessoaTeste.setAltura(1.62);
+    pessoaTeste.setPeso(55);
+    expect(pessoaTeste.calcular(),
+        'Valor do cáulculo: 20.957171162932475, resultado: saudável');
   });
 }
